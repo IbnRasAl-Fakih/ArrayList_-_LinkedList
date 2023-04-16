4b0258e7be7ea94d12c6328bd79aaa9a50130955
IbnRasAl-Fakih <rasul.eletai@gmail.com>
2023-04-16 15:21:20 +0600

    New methods: add(using index), contains, indexOf, remove, remove(using index)

--- MyList.java
+++ MyList.java
@@ -1,5 +1,10 @@
 public interface MyList<T> extends Iterable<T> {
     void add(T item);
+    void add(T item, int index);
     T get(int index);
     int size();
+    boolean contains(Object o);
+    int indexOf(Object o);
+    boolean remove(Object o);
+    T remove(int index);
 }
